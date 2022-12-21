c27f91579f959fc4e25a4bc1012d8c1df5d955d5
Daem Pasha <daempasha@gmail.com>
2022-12-21 23:05:53 +0000

    Added background to iamge

--- src/components/CardFront/index.tsx
+++ src/components/CardFront/index.tsx
@@ -1,21 +1,26 @@
+import BgCardFront from "./bg-card-front.png"
 import { addSpace, padString } from "./helper";
 import { CardFrontProps } from "./types";
 
 
-
 const CardFront: React.FC<CardFrontProps> = ({ className = "", name = "John Doe", number = "", expiryMonth = "00", expiryYear = "00", ...cardFrontProps }) => {
     return (
-        <div className={`shadow-md right-0 p-5 text-white flex flex-col bg-indigo-500 rounded-lg w-[400px] h-[200px] ${className}`
+        <div className={` overflow-hidden shadow-md right-0  text-white bg-indigo-500 rounded-lg w-[400px] h-[200px] ${className}`
         }>
-            <div className="flex items-center gap-3">
-                <div className="bg-white rounded-full w-10 h-10" />
-                <div className="border-2 border-white rounded-full w-5 h-5" />
-            </div>
-            <div className="flex text-3xl flex-grow items-center font-serif">{addSpace(padString(number, 16, "0"), 4)}</div>
-            <div className="flex justify-between">
-                <div className="font-serif">{name}</div>
-                <div className="font-serif">{`${expiryMonth}/${expiryYear}`}</div>
+            <img className="z-0 absolute top-0 left-0" src={BgCardFront} />
 
+            <div className="z-10 relative flex flex-col h-full p-5">
+
+                <div className="flex items-center gap-3">
+                    <div className="bg-white rounded-full w-10 h-10" />
+                    <div className="border-2 border-white rounded-full w-5 h-5" />
+                </div>
+                <div className="flex text-3xl flex-grow items-center font-serif">{addSpace(padString(number, 16, "0"), 4)}</div>
+                <div className="flex justify-between">
+                    <div className="font-serif">{name}</div>
+                    <div className="font-serif">{`${expiryMonth}/${expiryYear}`}</div>
+
+                </div>
             </div>
         </div >
     )
