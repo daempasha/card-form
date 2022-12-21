6c5ce224c4db09e8f1665eadec549c2c84694d67
Daem Pasha <daempasha@gmail.com>
2022-12-21 20:29:35 +0000

    Created background that changes depending on view size

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-
+import Card from "./components/Card/card.component"
 function App() {
 
   return (
-    <div className="App">
+    <div className="flex flex-col lg:flex-row h-screen w-screen">
+
+      <div className="block h-1/4 lg:h-full lg:w-1/4 bg-blue-500">
+
+      </div>
+      <div className="flex flex-col w-full h-full items-center justify-center">
+
+        <p>Cardholder name</p>
+        <p>Card number</p>
+        <p>Exp date CVC</p>
+        <button>Confirm</button>
+      </div>
     </div>
   )
 }
