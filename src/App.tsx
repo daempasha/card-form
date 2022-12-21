88aa1e4ed59e65e0e5d2c772868b417aaac40f45
Daem Pasha <daempasha@gmail.com>
2022-12-21 23:10:25 +0000

    Added backgrounds to sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import CardBack from "./components/CardBack"
 import CardFront from "./components/CardFront"
+import MainBgDesktop from "./bg-main-desktop.png"
+import MainBgMobile from "./bg-main-mobile.png"
 
 function App() {
 
   return (
     <div className="flex flex-col lg:flex-row h-screen w-screen">
-
       <div className="relative h-1/4 lg:h-full lg:w-1/2 bg-blue-500">
+        <img className="invisible lg:visible absolute left-0 top-0 h-full w-full" src={MainBgDesktop} />
+        <img className="visible lg:invisible absolute left-0 top-0 h-full w-full" src={MainBgMobile} />
+
         <CardFront className="invisible lg:visible absolute -right-24 bottom-1/2 my-5" />
         <CardBack className="invisible lg:visible absolute  -right-32 top-1/2 my-5 " />
       </div>
