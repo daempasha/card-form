b9822b4eb02f23a1893b7cd6e583ed07fae71a90
Daem Pasha <daempasha@gmail.com>
2022-12-22 15:11:24 +0000

    Design complete

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -14,24 +14,30 @@ const Form = () => {
         <form onSubmit={submitForm}>
             <div className="my-5">
                 <p className="uppercase text-xs tracking-widest mb-2 text-indigo-900 font-bold">Cardholder name</p>
-                <Input placeholder="e.g. John Doe" value={name} onChange={(event) => setName(event.target.value)} maxLength={16} />
+                <Input className="w-[400px]" placeholder="e.g. John Doe" value={name} onChange={(event) => setName(event.target.value)} maxLength={16} />
             </div>
 
             <div className="my-5">
                 <p className="uppercase text-xs tracking-widest mb-2 text-indigo-900 font-bold">Card number</p>
-                <Input value={number} type="tel" inputMode="numeric" onChange={(event) => setNumber(event.target.value)} maxLength={16} />
+                <Input className="w-[400px]" placeholder="e.g. 1234 4567 8534 2934" value={number} type="tel" inputMode="numeric" onChange={(event) => setNumber(event.target.value)} maxLength={16} />
+            </div>
+            <div className="my-5 flex gap-5">
+                <div>
+                    <p className="uppercase text-xs tracking-widest mb-2 text-indigo-900 font-bold">Expiry Date (MM/YY)</p>
+
+                    <Input value={expiryMonth} type="text" name="month" onChange={(event) => setExpiryMonth(event.target.value)} placeholder="MM" maxLength={2} size={2} />
+                    <span className="mx-1" />
+                    <Input value={expiryYear} type="text" name="year" onChange={(event) => setExpiryYear(event.target.value)} placeholder="YY" maxLength={2} size={2} />
+                </div>
+
+                <div className="flex-grow">
+                    <p className="uppercase text-xs tracking-widest mb-2 text-indigo-900 font-bold">CVC</p>
+                    <Input className="w-full" placeholder="e.g. 123" value={cvc} type="tel" inputMode="numeric" onChange={(event) => setCvc(event.target.value)} maxLength={3} />
+                </div>
             </div>
             <div className="my-5">
-                <p className="uppercase text-xs tracking-widest mb-2 text-indigo-900 font-bold">Expiry</p>
-                <Input value={expiryMonth} type="text" name="month" onChange={(event) => setExpiryMonth(event.target.value)} placeholder="MM" maxLength={2} size={2} />
-                <span className="mx-2" >/</span>
-                <Input value={expiryYear} type="text" name="year" onChange={(event) => setExpiryYear(event.target.value)} placeholder="YY" maxLength={2} size={2} />
 
-            </div>
-            <div className="my-5">
 
-                <p className="uppercase text-xs tracking-widest mb-2 text-indigo-900 font-bold">Cvc</p>
-                <Input value={cvc} type="tel" inputMode="numeric" onChange={(event) => setCvc(event.target.value)} maxLength={3} />
             </div>
 
             <Button>Confirm</Button>
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,7 +2,7 @@ import { InputProps } from "./types";
 
 const Input: React.FC<InputProps> = ({ className, ...inputProps }) => {
     return (
-        <input className={`border-[1px] px-3 py-1 border-gray-300 rounded-md outline-none focus:ring-2 ring-indigo-400 ${className}`} {...inputProps} />
+        <input className={`border-[1px] px-3 py-2 border-gray-300 rounded-md outline-none focus:ring-2 ring-indigo-400 ${className}`} {...inputProps} />
     )
 }
 
