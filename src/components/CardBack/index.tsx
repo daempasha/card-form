a5cac3880936185703eaff18eaa3d9f1c6bf450b
Daem Pasha <daempasha@gmail.com>
2022-12-25 18:04:18 +0000

    Fixed CardBack bg not displaying

--- src/components/CardBack/index.tsx
+++ src/components/CardBack/index.tsx
@@ -4,7 +4,7 @@ import { CardBackProps } from "./types";
 
 const CardBack: React.FC<CardBackProps> = ({ watch, className, cvc, ...cardFrontProps }) => {
     return (
-        <div className={`${RESPONSIVE_CARD} ${RESPONSIVE_CARD_BACK} ${BASE_CARD}bg-gray-200   ${className}`
+        <div className={`${RESPONSIVE_CARD} ${RESPONSIVE_CARD_BACK} ${BASE_CARD} bg-gray-200   ${className}`
         }>
             <div className="block h-6 md:h-12 lg:h-8 2xl:h-12 mt-5 mb-2 lg:mt-5 lg:mb-5 w-full bg-gray-700" />
 
