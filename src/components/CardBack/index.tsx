d51b49d9fe13d8e4a84661528727ca30f6c56c37
Daem Pasha <daempasha@gmail.com>
2022-12-25 16:43:01 +0000

    Added BASE_CARD styling

--- src/components/CardBack/index.tsx
+++ src/components/CardBack/index.tsx
@@ -1,9 +1,9 @@
-import { RESPONSIVE_CARD } from "@/styles";
+import { BASE_CARD, RESPONSIVE_CARD } from "@/styles";
 import { CardBackProps } from "./types";
 
 const CardBack: React.FC<CardBackProps> = ({ watch, className, cvc, ...cardFrontProps }) => {
     return (
-        <div className={` flex absolute bottom-4 lg:top-auto lg:bottom-1/3 xl:bottom-1/4 2xl:bottom-1/3  lg:-right-32 2xl:left-auto 2xl:-right-28 left-1/3 top-2 shadow-md text-white flex-col bg-gray-200 rounded-lg ${RESPONSIVE_CARD} ${className}`
+        <div className={`${RESPONSIVE_CARD} ${BASE_CARD} rounded-lg shadow-lg text-white flex absolute bottom-4 lg:top-auto lg:bottom-1/3 xl:bottom-1/4 2xl:bottom-1/3  lg:-right-32 2xl:left-auto 2xl:-right-28 left-1/3 top-2   flex-col bg-gray-200   ${className}`
         }>
             <div className="block h-6 md:h-12 lg:h-8 2xl:h-12 mt-5 mb-2 lg:mt-5 lg:mb-5 w-full bg-gray-700" />
 
